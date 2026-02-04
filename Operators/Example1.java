541981cff6771e919a99a73191a6ee6c352e4644
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-04 19:35:32 +0800

    Arithmetic operators usage
    Basic usage of arithmetic operators.

---
+++ Operators/Example1.java
@@ -0,0 +1,25 @@
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        
+       //Arithmetic Operators: +, -, *, /, %
+       //Basic usage of Operators
+       
+       int x = 16;
+       int y = 5;
+       
+       //Addition
+       System.out.println(x + y); //21
+       
+       //Subtraction
+       System.out.println(x - y); //11
+       
+       //Multiplication
+       System.out.println(x * y); //80
+       
+       //Division
+       System.out.println(x / y); //3
+       
+       //Modulo
+       System.out.println(x % y); //1
+    }
+}
