ef5396b479df49e3a8d2deca5e40cf6529700659
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-04 18:58:21 +0800

    Update and rename Example 1 to Example1.java

---
+++ Variables/Example1.java
@@ -0,0 +1,22 @@
+public class Example1 {
+    public static void main(String[] args) {
+        //Integer
+        int x = 10;
+        int y = 10;
+        System.out.println(x + y); //20
+        
+        //String
+        var First_Name = "Limitz";
+        var Last_Name = "Main";
+        
+        System.out.println(First_Name + Last_Name); //LimitzMain
+        
+        //Boolean
+        
+        boolean First_Arg = true;
+        boolean Second_Arg = false;
+        
+        System.out.println(First_Arg);
+        System.out.println(Second_Arg);
+    }
+}
