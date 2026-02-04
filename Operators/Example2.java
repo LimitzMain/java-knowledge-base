b229ae3a6febc26ee5630bba206b8e46db10a4ad
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-04 19:50:06 +0800

    Arithmetic operators usage with declaration

---
+++ Operators/Example2.java
@@ -0,0 +1,28 @@
+public class ArithDeclaration {
+    public static void main(String[] args) {
+       
+        //Arithmetic operators usage, with declaration.
+        
+        int x = 10;
+        int y = 10;
+        
+        //Addition
+        x += 5; // 15
+        
+        //Subtraction
+        y -= 10; // 0
+        
+        System.out.println("X value after addition: " + x);
+        System.out.println("Y value after subtraction: " + y);
+        
+        //Multiplcation
+        x *= 20; // 300
+        
+        System.out.println("X after multiplication: " + x);
+        
+        //Division
+        x /= 10; // 300 / 10 = 30
+        
+        System.out.println("X after division: " + x);
+    }
+}
