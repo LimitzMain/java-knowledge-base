3326214acc86b52ad478450ccc9d4ee9d1387752
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-04 19:27:34 +0800

    Storing/Declaring big numbers

--- Variables/Example3.java
+++ Variables/Example3.java
@@ -1,6 +1,6 @@
 public class StoringBigInt {
     public static void main(String[] args) {
-        int Int_Big_Number = (int) 1_000_000_000;
+        int Int_Big_Number = 1_000_000_000;
         System.out.println("Printing Big Number stored in an INT: " + Int_Big_Number);
         
         long Long_Big_Number = 1_000_000_000;
