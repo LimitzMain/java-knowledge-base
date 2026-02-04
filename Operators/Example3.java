5553b0ed12e6ce8a90d3c752f7a219a9f39946d7
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-04 19:56:14 +0800

    Basic conditional operators usage

---
+++ Operators/Example3.java
@@ -0,0 +1,9 @@
+public class Conditional {
+    public static void main(String[] args) {
+       //Conditional Operator
+       int a = 10;
+       String b = (a < 15) ? "Choice A" : "Choice B:";
+       
+       System.out.println(b); // Choice A
+    }
+}
