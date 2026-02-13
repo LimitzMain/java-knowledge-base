f4dae629cdfbbedbc57f58521bbfa1cafd19e5c4
Limitz <153339473+LimitzMain@users.noreply.github.com>
2026-02-14 00:38:13 +0800

    Simple conversion of String to Integer

---
+++ Variables/Example4.java
@@ -0,0 +1,10 @@
+public class Example4 {
+    public static void main(String[] args) {
+        String x = "15";
+        String y = "10";
+        
+        Integer.parseInt(x);
+        Integer.parseInt(y);
+        System.out.println(x + y);
+    }
+}
